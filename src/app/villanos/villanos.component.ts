75e662abb8a7bdb96f737442d3919a01b8064d6e
José Luis Reig Guerrero <joseluis.reig@alumnos.upm.es>
2024-03-13 19:15:29 +0100

    Botón villanos

---
+++ src/app/villanos/villanos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-villanos',
+  templateUrl: './villanos.component.html',
+  styleUrl: './villanos.component.css'
+})
+export class VillanosComponent {
+
+}
